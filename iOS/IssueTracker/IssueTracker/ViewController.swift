84fe1624c4a3c4b1c97395718b291cf7ca1b7e49
sihyungyou <sihyungyou@gmail.com>
2020-10-26 20:36:30 +0900

    chore :📦 프로젝트 생성

---
+++ iOS/IssueTracker/IssueTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IssueTracker
+//
+//  Created by sihyung you on 2020/10/26.
+//  Copyright © 2020 IssueTracker-15. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
