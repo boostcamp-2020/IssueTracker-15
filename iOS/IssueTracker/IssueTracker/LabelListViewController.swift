cd4623814b97e0da6b99bc9edc396f068dd8fd5e
sihyungyou <sihyungyou@gmail.com>
2020-10-27 17:09:47 +0900

    feat :⚡️ 레이블 화면 구현
    - 레이블 화면 CollectionView 적용하여 구현

--- iOS/IssueTracker/IssueTracker/LabelListViewController.swift
+++ iOS/IssueTracker/IssueTracker/LabelListViewController.swift
@@ -9,11 +9,58 @@
 import UIKit
 
 class LabelListViewController: UIViewController {
-
+    let tempLabels: [Label] = [Label(title: "feature", description: "기능추가", color: "#FF5D5D"),
+                               Label(title: "bug", description: "버그", color: "#96F879")]
+    
+    private lazy var collectionViewLayout: UICollectionViewFlowLayout = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: self.view.bounds.width, height: self.view.bounds.height / 10)
+        layout.headerReferenceSize = CGSize(width: self.view.bounds.width, height: self.view.bounds.height / 6.5)
+        layout.minimumInteritemSpacing = 0
+        layout.sectionHeadersPinToVisibleBounds = true
+        
+        return layout
+    }()
+    
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.setCollectionViewLayout(collectionViewLayout, animated: false)
+        LabelCellView.register(in: collectionView)
+        LabelHeaderView.register(in: collectionView)
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
 }
 
+extension LabelListViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return tempLabels.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = LabelCellView.dequeue(from: collectionView, for: indexPath) else { return UICollectionViewCell() }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        print("viewForSupplementaryElementOfKind")
+        guard let header = LabelHeaderView.dequeue(from: collectionView, for: indexPath) else {
+            print("guard")
+            return UICollectionReusableView()
+        }
+        
+        return header
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+}
