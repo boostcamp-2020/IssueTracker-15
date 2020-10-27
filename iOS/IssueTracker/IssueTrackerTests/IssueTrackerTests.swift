576515fa8d2cad6cfbe037feb0330791c2c49cfc
sihyungyou <sihyungyou@gmail.com>
2020-10-27 14:50:50 +0900

    feat :⚡️ Label model 생성

--- iOS/IssueTracker/IssueTrackerTests/IssueTrackerTests.swift
+++ iOS/IssueTracker/IssueTrackerTests/IssueTrackerTests.swift
@@ -18,17 +18,13 @@ class IssueTrackerTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testLabelInit() {
+        let label = Label(title: "title", description: "설명", color: "#ABABAB")
+        
+        XCTAssertEqual(label.title, "title")
+        XCTAssertEqual(label.description, "설명")
+        XCTAssertEqual(label.color, "#ABABAB")
     }
 
 }
