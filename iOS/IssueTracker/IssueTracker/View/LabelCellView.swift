e785ab15c30b4f73a3648a8140e5d24ca9c78069
sihyungyou <sihyungyou@gmail.com>
2020-10-27 17:05:59 +0900

    feat :⚡️ LabelCellView 구현
    - LabelCellView Xib 파일 생성
    - LabelCellView 클래스 생성
    - LabelCellView Extension 구현

---
+++ iOS/IssueTracker/IssueTracker/View/LabelCellView.swift
@@ -0,0 +1,16 @@
+//
+//  LabelCollectionViewCell.swift
+//  IssueTracker
+//
+//  Created by sihyung you on 2020/10/27.
+//  Copyright © 2020 IssueTracker-15. All rights reserved.
+//
+
+import UIKit
+
+class LabelCellView: UICollectionViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
