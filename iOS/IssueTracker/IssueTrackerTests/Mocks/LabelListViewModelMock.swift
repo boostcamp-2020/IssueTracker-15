5894101ee405e9abb7e1c54a7a651b3a1b16b143
SHINVVOO <sinsdhk@gmail.com>
2020-10-28 23:59:54 +0900

    chore   : :package: swiftLint 검사대상 test 제외 처리
    test    : :joystick: LabelListViewModel Mock 개체 생성
    test    : :joystick: LabelListViewControllerTest 파일 추가
    test    : :joystick: test를 위해 StoryBoard ViewController ID 설정

---
+++ iOS/IssueTracker/IssueTrackerTests/Mocks/LabelListViewModelMock.swift
@@ -0,0 +1,9 @@
+//
+//  LabelListViewModelMock.swift
+//  IssueTrackerTests
+//
+//  Created by 김신우 on 2020/10/28.
+//  Copyright © 2020 IssueTracker-15. All rights reserved.
+//
+
+import Foundation
---
+++ iOS/IssueTracker/IssueTrackerTests/Tests/LabelListViewControllerTests.swift
@@ -0,0 +1,33 @@
+//
+//  LabelListViewControllerTests.swift
+//  IssueTrackerTests
+//
+//  Created by 김신우 on 2020/10/28.
+//  Copyright © 2020 IssueTracker-15. All rights reserved.
+//
+
+import XCTest
+
+class LabelListViewControllerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
