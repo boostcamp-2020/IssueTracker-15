44c915d575379d21f9b7b8c5a9cbe0182efe8588
SHINVVOO <sinsdhk@gmail.com>
2020-10-29 00:02:24 +0900

    test    : :joystick: viewDidLoad에서 Mock의 needFetch 호출 테스트

--- iOS/IssueTracker/IssueTrackerTests/Mocks/LabelListViewModelMock.swift
+++ iOS/IssueTracker/IssueTrackerTests/Mocks/LabelListViewModelMock.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+@testable import IssueTracker
+
+class LabelListViewModelMock: IssueTracker.LabelListViewModelProtocol {
+    
+    var didFetch: (() -> Void)?
+    
+    var needFetchItemsCalled = false
+    var cellForItemAtCalled = false
+    var numberOfItemCalled = false
+    var addNewLabelCalled = false
+    var editLabelCalled = false
+    
+    func needFetchItems() {
+        needFetchItemsCalled = true
+    }
+    
+    func cellForItemAt(path: IndexPath) -> IssueTracker.LabelItemViewModel {
+        cellForItemAtCalled = true
+        return IssueTracker.LabelItemViewModel(label: IssueTracker.Label(title: "A", description: "B", hexColor: "C"))
+    }
+    
+    func numberOfItem() -> Int {
+        numberOfItemCalled = true
+        return 1
+    }
+    
+    func addNewLabel(title: String, desc: String, hexColor: String) {
+        addNewLabelCalled = true
+    }
+    
+    func editLabel(at indexPath: IndexPath, title: String, desc: String, hexColor: String) {
+        editLabelCalled = true
+    }
+    
+}
--- iOS/IssueTracker/IssueTrackerTests/Tests/LabelListViewControllerTests.swift
+++ iOS/IssueTracker/IssueTrackerTests/Tests/LabelListViewControllerTests.swift
@@ -7,27 +7,27 @@
 //
 
 import XCTest
+@testable import IssueTracker
 
 class LabelListViewControllerTests: XCTestCase {
+    
+    let mockViewModel = LabelListViewModelMock()
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func makeSUT() -> IssueTracker.LabelListViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        
+        let vc = storyboard.instantiateViewController(withIdentifier: "LabelListVC") as! IssueTracker.LabelListViewController
+        vc.labelListViewModel = mockViewModel
+        vc.loadView()
+        return vc
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testNeedFetchLabelsCalled() {
+        let vc = makeSUT()
+        
+        vc.viewDidLoad()
+        
+        XCTAssertTrue(mockViewModel.needFetchItemsCalled)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
 }
