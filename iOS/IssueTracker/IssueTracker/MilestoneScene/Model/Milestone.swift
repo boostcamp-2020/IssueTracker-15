bebf13a84915339eb73b4aa95f3eaf8cc489f26a
SHINVVOO <sinsdhk@gmail.com>
2020-10-29 11:20:25 +0900

    fix     : :fire: Xcode 12버전 오류

---
+++ iOS/IssueTracker/IssueTracker/MilestoneScene/Model/Milestone.swift
@@ -0,0 +1,16 @@
+//
+//  Milestone.swift
+//  IssueTracker
+//
+//  Created by 김신우 on 2020/10/29.
+//  Copyright © 2020 IssueTracker-15. All rights reserved.
+//
+
+import Foundation
+
+struct Milestone {
+    let id: Int
+    let title: String
+    let description: String
+    
+}
