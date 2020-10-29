016fc06b3407cb7e41f6e914f7fdd965ea11ca48
sihyungyou <sihyungyou@gmail.com>
2020-10-29 14:15:28 +0900

    feat :⚡️ 마일스톤 목록화면 헤더 구현
    - 레이블 목록화면의 헤더 xib 파일을 재사용하여 마일스톤 목록화면 구성

--- iOS/IssueTracker/IssueTracker/MilestoneScene/View/MilestoneListViewController.swift
+++ iOS/IssueTracker/IssueTracker/MilestoneScene/View/MilestoneListViewController.swift
@@ -21,14 +21,14 @@ class MilestoneListViewController: UIViewController {
         setupCollectionViewLayout()
         collectionView.delegate = self
         collectionView.dataSource = self
-        MilestoneHeaderView.register(in: collectionView)
         MilestoneCellView.register(in: collectionView)
+        HeaderView.register(in: collectionView)
     }
     
     private func setupCollectionViewLayout() {
         let layout = UICollectionViewFlowLayout()
         layout.itemSize = CGSize(width: self.view.bounds.width, height: self.view.bounds.height / 10)
-        layout.headerReferenceSize = CGSize(width: self.view.bounds.width, height: self.view.bounds.height / 6.5)
+        layout.headerReferenceSize = CGSize(width: self.view.bounds.width, height: self.view.bounds.height / 12)
         layout.minimumLineSpacing = 1
         layout.sectionHeadersPinToVisibleBounds = true
         collectionView.setCollectionViewLayout(layout, animated: false)
@@ -51,9 +51,10 @@ extension MilestoneListViewController: UICollectionViewDataSource, UICollectionV
     }
     
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        guard let header = MilestoneHeaderView.dequeue(from: collectionView, for: indexPath) else { return UICollectionReusableView() }
+        guard let header = HeaderView.dequeue(from: collectionView, for: indexPath) else { return UICollectionReusableView() }
         
         header.configure(title: "마일스톤")
+        
         return header
     }
 }
