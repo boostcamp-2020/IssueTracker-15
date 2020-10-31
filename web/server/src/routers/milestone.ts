a60e18719b63d6449a46a8ba29132a5869d4aabf
문혜라 <MoonHyeRa@munhyelaui-MacBook.local>
2020-10-31 20:14:05 +0900

    feat    : :zap:️ milestone add 라우터 추가
    1. milestone.types.ts 에서 req type 정의
    2. 이후 newMilestoneData 변수 ( req.body 받아서 저장하는 변수) 에 타입 설정
    )

--- web/server/src/routers/milestone.ts
+++ web/server/src/routers/milestone.ts
@@ -1,5 +1,13 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import MilestoneEntity from "../entity/issue.entity";
+import { createMilestone } from "../types/milestone.types";
 
 const MilestoneRouter = express.Router();
+MilestoneRouter.post("/", async (req: Request, res: Response) => {
+  const newMilestoneData: createMilestone = req.body;
 
+  try {
+    res.json("hello");
+  } catch (e) {}
+});
 export default MilestoneRouter;
---
+++ web/server/src/types/milestone.types.ts
@@ -0,0 +1,5 @@
+export interface createMilestone {
+  title: string;
+  description?: string;
+  dueDate?: Date;
+}
