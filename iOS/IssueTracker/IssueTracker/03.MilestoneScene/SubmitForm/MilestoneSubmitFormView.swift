6a4f477e07c1cf056a6524fb7b5b2d9f75bfa70e
sihyungyou <sihyungyou@gmail.com>
2020-10-31 15:38:36 +0900

    feat :⚡️ showAlert 함수 기능 축소
    - showAlert는 전달받은 title을 띄울 뿐이지 기존 함수처럼 내부에서 submitFormView의
      origin을 조작하는 것은 부자연스러움
    - 따라서 showAlert는 전달받는 title을 표시하되, formView를 키보드를 고려해 위/아래로
      내려야 한다면 prepare/completion handler를 전달받도록 함수를 변경
    - prepare이 있는 이유는 alert 창이 뜨면서 키보드가 내려가는데 그 때 formView를
      아래로 내리기 위함
    - completion은 alert의 ok 버튼을 누른 후에 다시 키보드가 올라와야하므로 formView
      를 위로 올리기 위함
    - prepare/compeltion은 모두 optional closure 타입으로 일반적인 showAlert가 필요한
      상황이라면 nil로 전달하면 됨

--- iOS/IssueTracker/IssueTracker/03.MilestoneScene/SubmitForm/MilestoneSubmitFormView.swift
+++ iOS/IssueTracker/IssueTracker/03.MilestoneScene/SubmitForm/MilestoneSubmitFormView.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class MilestoneSubmitFormView: UIView {
     var formViewEndPoint: CGFloat?
+    var moveUpward: CGFloat?
     var saveButtonTapped: ((String, String, String) -> Void)?
     @IBOutlet weak var formView: UIView!
     @IBOutlet weak var backgroundView: UIView!
@@ -38,17 +39,27 @@ class MilestoneSubmitFormView: UIView {
     
     @IBAction func saveButtonTapped(_ sender: UIButton) {
         guard checkDateFieldValidation() else {
-            self.showAlert(title: "날짜를 양식에 맞게 적어주세요!", prepare: nil, completion: nil)
+            self.showAlert(title: "날짜를 양식에 맞게 적어주세요!", prepare: movewFormViewDownward, completion: moveFormViewUpward)
             return
         }
         
         guard let titleText = titleField.text, !titleText.isEmpty else {
-            self.showAlert(title: "제목은 반드시 입력해야해요!", prepare: nil, completion: nil)
+            self.showAlert(title: "제목은 반드시 입력해야해요!", prepare: movewFormViewDownward, completion: moveFormViewUpward)
             return
         }
         
         saveButtonTapped?(titleText, descField?.text ?? "", dateField?.text ?? "")
         self.removeFromSuperview()
+    }
+    
+    private func moveFormViewUpward() {
+        guard let moveUpward = moveUpward else { return }
+        formView.frame.origin.y -= moveUpward
+    }
+    
+    private func movewFormViewDownward() {
+        guard let moveUpward = moveUpward else { return }
+        formView.frame.origin.y += moveUpward
     }
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {
