82d16f4caea12ffe23240d617da1aebcaff4e88e
moaikang <kk_w1207@naver.com>
2020-11-02 23:27:56 +0900

    feat    : :zap:️ API : POST /api/comment
    comment를 추가하는 API를 생성하였다.

--- web/server/src/routers/comment.ts
+++ web/server/src/routers/comment.ts
@@ -1,5 +1,18 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { getRepository, Repository } from "typeorm";
+import CommentEntity from "../entity/comment.entity";
+import CommentService from "../services/comment.service";
 
 const CommentRouter = express.Router();
+
+CommentRouter.post('/', async (req: Request, res: Response) => {
+  try {
+    const { body } = req;
+    const result = await CommentService.create(body);
+    res.status(200).json({ status: 200, message: "OK", data: result })
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+})
 
 export default CommentRouter;
---
+++ web/server/src/services/comment.service.ts
@@ -0,0 +1,14 @@
+import { getRepository, Repository } from "typeorm";
+import CommentEntity from "../entity/comment.entity";
+
+const CommentService = {
+  create: async (body: CommentEntity): Promise<CommentEntity> => {
+    const commentRepository: Repository<CommentEntity> = getRepository(CommentEntity);
+    const comment: CommentEntity = await commentRepository.create(body)
+    const result: CommentEntity = await commentRepository.save(comment);
+    return result;
+  }
+
+}
+
+export default CommentService;
