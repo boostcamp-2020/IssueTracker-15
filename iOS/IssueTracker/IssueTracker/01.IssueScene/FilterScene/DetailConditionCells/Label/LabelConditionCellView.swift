b20ad28d89b29939402ff5597dee242ff10e5317
SHINVVOO <sinsdhk@gmail.com>
2020-11-03 01:39:30 +0900

    feat	: :zap: LabelConditionCellView 기본 구성

--- iOS/IssueTracker/IssueTracker/01.IssueScene/FilterScene/DetailConditionCells/Label/LabelConditionCellView.swift
+++ iOS/IssueTracker/IssueTracker/01.IssueScene/FilterScene/DetailConditionCells/Label/LabelConditionCellView.swift
@@ -9,16 +9,39 @@
 import UIKit
 
 class LabelConditionCellView: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    enum Constant {
+        static let imageOnSelected = UIImage(systemName: "plus.circle")
+        static let imageOutSelected = UIImage(systemName: "x.circle.fill")
+        static let colorOnSelected = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+        static let colorOutSelected = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    @IBOutlet weak var labelBadge: BadgeLabelView!
+    @IBOutlet weak var accessoryImage: UIImageView!
 
-        // Configure the view for the selected state
+    // TODO: LabelViewModel
+     func configure() {
+         // 임시
+         labelBadge.text = "Feature"
+     }
+     
+     var select : Bool = false {
+         didSet {
+             accessoryImage.image = isSelected ? Constant.imageOnSelected : Constant.imageOutSelected
+             accessoryImage.tintColor = isSelected ? Constant.colorOnSelected : Constant.colorOutSelected
+         }
+     }
+     
+}
+
+// MARK: - UITableViewRegistable Implementation
+extension LabelConditionCellView: UITableViewRegisterable {
+    static var cellIdentifier: String {
+        return "LabelConditionCellView"
     }
     
+    static var cellNib: UINib {
+        return UINib(nibName: cellIdentifier, bundle: nil)
+    }
 }
