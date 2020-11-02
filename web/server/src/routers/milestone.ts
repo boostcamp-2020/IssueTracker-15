7c6c0b4de728e4fb270a2bd4fef5c56e806c52f8
Hera <maong0927@gmail.com>
2020-11-02 12:52:39 +0900

    feat    : :zap:️ milestone create 라우터 작성
    1. 성공시 200 반환
    2. 실패시 에러메시지와 400 코드 반환

--- web/server/src/routers/milestone.ts
+++ web/server/src/routers/milestone.ts
@@ -1,5 +1,16 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import MilestoneService from "../services/milestone.service";
+import { Milestone } from "../types/milestone.types";
 
 const MilestoneRouter = express.Router();
+MilestoneRouter.post("/", async (req: Request, res: Response) => {
+  const newMilestoneData: Milestone = req.body;
 
+  try {
+    await MilestoneService.createMilestone(newMilestoneData);
+    return res.status(200);
+  } catch (e) {
+    return res.status(400).json(e.message);
+  }
+});
 export default MilestoneRouter;
