a97b387b1f20714fb5340ae8e5e93435f4f6bfa6
moaikang <kk_w1207@naver.com>
2020-11-03 00:16:03 +0900

    fix     : :fire: 암호화 관련 횟수 코드에서 안보이게 수정
       암호화 하는 횟수를 환경변수로 설정한 뒤, 이를 코드에서 사용하는 방식을 사용해
       암호의 보안을 높게 하였다.

--- web/server/src/lib/encryption.ts
+++ web/server/src/lib/encryption.ts
@@ -1,8 +1,11 @@
 import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const Encryption = {
   encryptPassword: async (password: string): Promise<string> => {
-    const hashedPassword = await bcrypt.hash(password, 13);
+    const hashedPassword = await bcrypt.hash(password, parseInt(process.env.ENCRYPT_TIMES as string));
     return hashedPassword;
   }
 }
