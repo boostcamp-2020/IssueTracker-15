576557f0842851a5e29f8db2fbcb059a3119f6ae
SHINVVOO <sinsdhk@gmail.com>
2020-11-02 20:46:54 +0900

    refactor: :rainbow: label,milestone model api model과 매칭

--- iOS/IssueTracker/IssueTracker/02.LabelScene/ViewModel/LabelListViewModel.swift
+++ iOS/IssueTracker/IssueTracker/02.LabelScene/ViewModel/LabelListViewModel.swift
@@ -23,31 +23,31 @@ class LabelListViewModel: LabelListViewModelProtocol {
     private var labels = [Label]()
     
     func editLabel(at indexPath: IndexPath, title: String, desc: String, hexColor: String) {
-        labels[indexPath.row] = Label(title: title, description: desc, hexColor: hexColor)
+        labels[indexPath.row] = Label(id: 0, title: title, description: desc, hexColor: hexColor)
         
         didFetch?()
     }
     
     func addNewLabel(title: String, desc: String, hexColor: String) {
-        let newLabel: Label = Label(title: title, description: desc, hexColor: hexColor)
+        let newLabel: Label = Label(id: 0, title: title, description: desc, hexColor: hexColor)
         labels.insert(newLabel, at: 0)
         
         didFetch?()
     }
     
     func needFetchItems() {
-        labels = [Label(title: "feature", description: "기능에 대한 레이블입니다.", hexColor: "#FF5D5D"),
-                  Label(title: "bug", description: "수정할 버그에 대한 레이블입니다.", hexColor: "#96F879"),
-                  Label(title: "feature", description: "기능에 대한 레이블입니다.", hexColor: "#FF5D5D"),
-                  Label(title: "bug", description: "수정할 버그에 대한 레이블입니다.", hexColor: "#96F879"),
-                  Label(title: "feature", description: "기능에 대한 레이블입니다.", hexColor: "#FF5D5D"),
-                  Label(title: "bug", description: "수정할 버그에 대한 레이블입니다.", hexColor: "#96F879"),
-                  Label(title: "feature", description: "기능에 대한 레이블입니다.", hexColor: "#FF5D5D"),
-                  Label(title: "bug", description: "수정할 버그에 대한 레이블입니다.", hexColor: "#96F879"),
-                  Label(title: "feature", description: "기능에 대한 레이블입니다.", hexColor: "#FF5D5D"),
-                  Label(title: "bug", description: "수정할 버그에 대한 레이블입니다.", hexColor: "#96F879"),
-                  Label(title: "feature", description: "기능에 대한 레이블입니다.", hexColor: "#FF5D5D"),
-                  Label(title: "bug", description: "수정할 버그에 대한 레이블입니다.", hexColor: "#96F879")]
+        labels = [Label(id: 0, title: "feature", description: "기능에 대한 레이블입니다.", hexColor: "#FF5D5D"),
+                  Label(id: 0, title: "bug", description: "수정할 버그에 대한 레이블입니다.", hexColor: "#96F879"),
+                  Label(id: 0, title: "feature", description: "기능에 대한 레이블입니다.", hexColor: "#FF5D5D"),
+                  Label(id: 0, title: "bug", description: "수정할 버그에 대한 레이블입니다.", hexColor: "#96F879"),
+                  Label(id: 0, title: "feature", description: "기능에 대한 레이블입니다.", hexColor: "#FF5D5D"),
+                  Label(id: 0, title: "bug", description: "수정할 버그에 대한 레이블입니다.", hexColor: "#96F879"),
+                  Label(id: 0, title: "feature", description: "기능에 대한 레이블입니다.", hexColor: "#FF5D5D"),
+                  Label(id: 0, title: "bug", description: "수정할 버그에 대한 레이블입니다.", hexColor: "#96F879"),
+                  Label(id: 0, title: "feature", description: "기능에 대한 레이블입니다.", hexColor: "#FF5D5D"),
+                  Label(id: 0, title: "bug", description: "수정할 버그에 대한 레이블입니다.", hexColor: "#96F879"),
+                  Label(id: 0, title: "feature", description: "기능에 대한 레이블입니다.", hexColor: "#FF5D5D"),
+                  Label(id: 0, title: "bug", description: "수정할 버그에 대한 레이블입니다.", hexColor: "#96F879")]
         didFetch?()
     }
     
--- iOS/IssueTracker/IssueTracker/03.MilestoneScene/Model/Milestone.swift
+++ iOS/IssueTracker/IssueTracker/03.MilestoneScene/Model/Milestone.swift
@@ -8,10 +8,56 @@
 
 import Foundation
 
-struct Milestone {
+struct Milestone: Codable {
     let id: Int
     let title: String
     let description: String
+    let openIssuesLength: Int
+    let closeIssueLength: Int
     let dueDate: String
-    // TODO:- Milestone에 포함되어 있는 Issue들에 대한 정보 (미정)
+    
+    init(id: Int, title: String, description: String, dueDate: String, openIssuesLength: Int, closeIssueLength: Int) {
+        self.id = id
+        self.title = title
+        self.description = description
+        self.dueDate = dueDate
+        self.openIssuesLength = openIssuesLength
+        self.closeIssueLength = closeIssueLength
+    }
+    
+    init(id: Int, title: String,description: String, dueDate: String) {
+        self.init(id: id, title: title, description: description, dueDate: dueDate, openIssuesLength: 10, closeIssueLength: 10)
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: DeCodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        title = try container.decode(String.self, forKey: .title)
+        description = try container.decode(String.self, forKey: .description)
+        openIssuesLength = try container.decode(Int.self, forKey: .openIssueLength)
+        closeIssueLength = try container.decode(Int.self, forKey: .closeIssueLength)
+        dueDate = try container.decode(String.self, forKey: .dueDate)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: EnCodingKeys.self)
+        try container.encode(title, forKey: .title)
+        try container.encode(description, forKey: .description)
+        try container.encode(dueDate, forKey: .dueDate)
+    }
+    
+    enum EnCodingKeys: CodingKey {
+        case title
+        case dueDate
+        case description
+    }
+    
+    enum DeCodingKeys: String, CodingKey {
+        case id
+        case title
+        case description
+        case openIssueLength = "OpenIssuesLength"
+        case closeIssueLength = "CloseIssuesLength"
+        case dueDate
+    }
 }
