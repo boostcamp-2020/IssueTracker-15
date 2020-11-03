514dde2cb8f93ead986196c49e1e59ce487526d8
moaikang <kk_w1207@naver.com>
2020-11-03 12:10:29 +0900

    refactor: :rainbow: 회원가입 API refactor
       회의에서 정한 response 규칙에 맞게 리팩토링했다.
       그리고 result -> newUser와 같이 변수를 명시적으로 리팩토링했다.

--- web/server/src/routers/auth/sign-up.ts
+++ web/server/src/routers/auth/sign-up.ts
@@ -1,17 +1,22 @@
-import express, { Request, Response } from "express";
-import SignUpService from "../../services/sign-up.service";
+import express, { Request, Response } from 'express';
+import SignUpService from '../../services/sign-up.service';
 
 const SignUpRouter = express.Router();
 
 SignUpRouter.post('/', async (req: Request, res: Response) => {
   const { user_id, password, userName } = req.body;
   try {
-    const result = await SignUpService.create(user_id, password, userName, 'local');
-    console.log(result);
-    res.status(200).json({ status: 200, message: "OK" });
+    const result = await SignUpService.create(
+      user_id,
+      password,
+      userName,
+      'local'
+    );
+
+    return res.status(200).json();
   } catch (err) {
-    res.status(400).json({ status: 400, message: err.message })
+    return res.status(400).json({ message: err.message });
   }
-})
+});
 
 export default SignUpRouter;
--- web/server/src/services/sign-up.service.ts
+++ web/server/src/services/sign-up.service.ts
@@ -1,16 +1,26 @@
-import { getRepository } from "typeorm";
-import Encryption from '../lib/encryption'
-import UserEntity from "../entity/user.entity";
+import { getRepository } from 'typeorm';
+import Encryption from '../lib/encryption';
+import UserEntity from '../entity/user.entity';
 
 const SignUpService = {
-  create: async (email: string, password: string, userName: string, type: string): Promise<UserEntity> => {
+  create: async (
+    email: string,
+    password: string,
+    userName: string,
+    type: string
+  ): Promise<UserEntity> => {
     const hashedPassword = await Encryption.encryptPassword(password);
 
-    const userRepository = getRepository(UserEntity)
-    const user: UserEntity = await userRepository.create({ email, password: hashedPassword, userName, type });
-    const result: UserEntity = await userRepository.save(user);
-    return result;
-  }
-}
+    const userRepository = getRepository(UserEntity);
+    const user: UserEntity = await userRepository.create({
+      email,
+      password: hashedPassword,
+      userName,
+      type,
+    });
+    const newUser: UserEntity = await userRepository.save(user);
+    return newUser;
+  },
+};
 
 export default SignUpService;
