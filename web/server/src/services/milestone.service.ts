a1372dda23b1a7b09c0970e8daa54347194bb789
Hera <maong0927@gmail.com>
2020-11-03 11:42:45 +0900

    refactor: :rainbow: db 접근 함수 변경
    1. insert -> create & save
    2. remove -> delete

--- web/server/src/services/milestone.service.ts
+++ web/server/src/services/milestone.service.ts
@@ -2,12 +2,11 @@ import { getRepository, Repository } from "typeorm";
 import MilestoneEntity from "../entity/milestone.entity";
 import { Milestone } from "../types/milestone.types";
 
-const milestoneRepository: Repository<MilestoneEntity> = getRepository(
-  MilestoneEntity
-);
-
 const MilestoneService = {
   getMilestones: async (): Promise<Record<string, string>> => {
+    const milestoneRepository: Repository<MilestoneEntity> = getRepository(
+      MilestoneEntity
+    );
     const result = await milestoneRepository.query(
       `SELECT count(if(Issue.isOpend,1,null)) as openedIssueNum,count(if(Issue.isOpend=0,1,null)) as closedIssueNum,  Milestone.* FROM Issue RIGHT OUTER JOIN Milestone ON Issue.milestoneId=Milestone.id group by Milestone.id`
     );
@@ -15,24 +14,39 @@ const MilestoneService = {
   },
   createMilestone: async (
     milestoneData: Milestone
-  ): Promise<Record<string, number>> => {
-    const result = await milestoneRepository.insert(milestoneData);
-    return result.identifiers[0];
+  ): Promise<MilestoneEntity> => {
+    const milestoneRepository: Repository<MilestoneEntity> = getRepository(
+      MilestoneEntity
+    );
+    const milestone: MilestoneEntity = await milestoneRepository.create(
+      milestoneData
+    );
+    const newMilestone: MilestoneEntity = await milestoneRepository.save(
+      milestone
+    );
+
+    return newMilestone;
   },
 
   deleteMilestone: async (milestoneId: number): Promise<void> => {
+    const milestoneRepository: Repository<MilestoneEntity> = getRepository(
+      MilestoneEntity
+    );
     const milestoneToRemove:
       | MilestoneEntity
       | undefined = await milestoneRepository.findOne(milestoneId);
     if (!milestoneToRemove)
       throw new Error(`can't find milestone id ${milestoneId}`);
-    await milestoneRepository.remove(milestoneToRemove);
+    await milestoneRepository.delete(milestoneToRemove.id);
   },
 
   updateMilestone: async (
     milestoneId: number,
     milestoneData: Milestone
   ): Promise<void> => {
+    const milestoneRepository: Repository<MilestoneEntity> = getRepository(
+      MilestoneEntity
+    );
     const milestoneToUpdate:
       | MilestoneEntity
       | undefined = await milestoneRepository.findOne(milestoneId);
