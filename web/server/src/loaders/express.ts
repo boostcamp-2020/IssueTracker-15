d526bc88de967a2d2b16d5c82736b2645ba394e1
박진용 <jinyoung1997@gmail.com>
2020-11-03 21:06:26 +0900

    refactor : :rainbow: issue entity column 오타 수정
    isopend -> isopened

--- web/server/src/loaders/express.ts
+++ web/server/src/loaders/express.ts
@@ -1,8 +1,8 @@
-import express, { NextFunction, Request, Response } from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import APIRouter from '../routers';
-import createDBConnection from './database';
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import APIRouter from "../routers";
+import createDBConnection from "./database";
 
 export default async ({ app }: { app: express.Application }): Promise<void> => {
   await createDBConnection();
@@ -10,7 +10,7 @@ export default async ({ app }: { app: express.Application }): Promise<void> => {
   app.use(cors());
   app.use(bodyParser.json());
 
-  app.use('/api', APIRouter);
+  app.use("/api", APIRouter);
 
   app.use((error: any, req: Request, res: Response, next: NextFunction) => {
     console.error(error);
