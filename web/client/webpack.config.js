a4672cdbb58533d78d0809ee86a1aeef689df882
박진용 <jinyoung1997@gmail.com>
2020-11-03 18:27:44 +0900

    chore   : í³¦ babel-loader add

--- web/client/webpack.config.js
+++ web/client/webpack.config.js
@@ -1,5 +1,18 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
+  entry: "./src/index.ts",
+  resolve: {
+    extensions: [".js", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
