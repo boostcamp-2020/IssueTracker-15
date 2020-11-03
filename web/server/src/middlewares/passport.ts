07dacbfe5ce017a1dc6817975ea057c4718a6500
moaikang <kk_w1207@naver.com>
2020-11-03 12:13:01 +0900

    feat    : :zap:️ passport Middleware
    유저를 토큰으로 판별하고, 로그인을 하게 도와주는 패스포트 미들웨어를 만들었다.

---
+++ web/server/src/middlewares/passport.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from 'express';
+import passport from 'passport';
+
+const PassportMiddleware = {
+  validateUser: async (req: Request, res: Response, next: NextFunction) => {
+    passport.authenticate('jwt', async (err, user, info) => {
+      if (err || info) return res.status(400).json({ message: info.message });
+
+      req.user = user;
+      next();
+    })(req, res, next);
+  },
+
+  signIn: async (req: Request, res: Response, next: NextFunction) => {
+    passport.authenticate('local', async (err, user, info) => {
+      if (err || info) return res.status(400).json({ status: 400, message: err.message || info.message });
+
+      req.login(user, { session: false }, async loginErr => {
+        next(loginErr);
+      });
+
+      req.user = user;
+      next();
+    })(req, res, next);
+  }
+}
+
+export default PassportMiddleware;
