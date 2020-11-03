ec237f79a33e794f375a0f12f730adf3475f67a4
moaikang <kk_w1207@naver.com>
2020-11-03 11:58:11 +0900

    refactor: :rainbow: Comment API refactor
    Comment Post API를 response 컨벤션 정한대로 리팩토링했다.
    또한, 변수의 이름을 result -> newComment로 더욱 의미있게 바꾸었다.

--- web/server/src/routers/comment.ts
+++ web/server/src/routers/comment.ts
@@ -1,18 +1,18 @@
-import express, { Request, Response } from "express";
-import { getRepository, Repository } from "typeorm";
-import CommentEntity from "../entity/comment.entity";
-import CommentService from "../services/comment.service";
+import express, { Request, Response } from 'express';
+import { getRepository, Repository } from 'typeorm';
+import CommentEntity from '../entity/comment.entity';
+import CommentService from '../services/comment.service';
 
 const CommentRouter = express.Router();
 
 CommentRouter.post('/', async (req: Request, res: Response) => {
   try {
     const { body } = req;
-    const result = await CommentService.create(body);
-    res.status(200).json({ status: 200, message: "OK", data: result })
+    const newComment = await CommentService.create(body);
+    res.status(200).json({ comment: newComment });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
-})
+});
 
 export default CommentRouter;
