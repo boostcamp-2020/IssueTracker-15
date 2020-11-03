e0f4d34b8947418833d1d65035124caf2cf8905e
박진용 <jinyoung1997@gmail.com>
2020-11-03 18:41:18 +0900

    chore   : í³¦ add react

--- web/client/webpack.config.js
+++ web/client/webpack.config.js
@@ -1,6 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  entry: "./src/index.ts",
+  entry: "./src/index.tsx",
   resolve: {
     extensions: [".js", ".ts", ".tsx"],
   },
