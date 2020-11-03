9eb438c2d01f69beb654d136715678ed12ad553e
박진용 <jinyoung1997@gmail.com>
2020-11-04 02:46:33 +0900

    feat    : ⚡ get comment by issueId method 구현

--- web/server/src/services/comment.service.ts
+++ web/server/src/services/comment.service.ts
@@ -1,5 +1,5 @@
-import { getRepository, Repository } from 'typeorm';
-import CommentEntity from '../entity/comment.entity';
+import { getRepository, Repository } from "typeorm";
+import CommentEntity from "../entity/comment.entity";
 
 const CommentService = {
   create: async (body: CommentEntity): Promise<CommentEntity> => {
@@ -9,6 +9,24 @@ const CommentService = {
     const comment: CommentEntity = await commentRepository.create(body);
     const newComment: CommentEntity = await commentRepository.save(comment);
     return newComment;
+  },
+
+  getCommentsByIssueId: async (issueId: number) => {
+    const commentRepository = getRepository(CommentEntity);
+    const comments = await commentRepository
+      .createQueryBuilder("Comment")
+      .innerJoin("Comment.user", "User")
+      .where("Comment.issueId = :issueId", { issueId })
+      .select([
+        "Comment.id",
+        "Comment.content",
+        "Comment.createAt",
+        "User.userName",
+        "User.imageURL",
+      ])
+      .getMany();
+
+    return comments;
   },
 };
 
