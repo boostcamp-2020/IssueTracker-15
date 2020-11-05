3e8f43d3dbe452c1e52710cafd9b16c69b4ff0fb
moaikang <kk_w1207@naver.com>
2020-11-05 15:45:37 +0900

    feat    : :zap:️ CSS Reset
    styled-reset 라이브러리를 이용해 CSS를 초기화했다.

--- web/client/src/app.tsx
+++ web/client/src/app.tsx
@@ -1,23 +1,29 @@
-import React from "react";
-import { Redirect, Route, Switch } from "react-router-dom";
-import CreateIssuePage from "./views/create-issue";
-import CreateMilestonePage from "./views/create-milestone";
-import DetailIssuePage from "./views/detail-issue";
-import IssuePage from "./views/issue";
-import LabelPage from "./views/label";
-import MilestonePage from "./views/milestone";
+import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { Reset } from 'styled-reset';
+import Commentbox from './components/commentBox/CommentBox';
+import IssueHeader from './components/IssueHeader/IssueHeader';
+import CreateIssuePage from './views/create-issue';
+import CreateMilestonePage from './views/create-milestone';
+import DetailIssuePage from './views/detail-issue';
+import IssuePage from './views/issue';
+import LabelPage from './views/label';
+import MilestonePage from './views/milestone';
 
 const App = () => {
   return (
-    <Switch>
-      <Route path="/milestone/new" component={CreateMilestonePage} />
-      <Route path="/milestone" component={MilestonePage} />
-      <Route path="/label" component={LabelPage} />
-      <Route path="/issue/new" exact={true} component={CreateIssuePage} />
-      <Route path="/issue/:id" component={DetailIssuePage} />
-      <Route path="/issue" exact={true} component={IssuePage} />
-      <Redirect from="*" to="/issue" />
-    </Switch>
+    <>
+      <Reset />
+      <Switch>
+        <Route path="/milestone/new" component={CreateMilestonePage} />
+        <Route path="/milestone" component={MilestonePage} />
+        <Route path="/label" component={LabelPage} />
+        <Route path="/issue/new" exact={true} component={CreateIssuePage} />
+        <Route path="/issue/:id" component={DetailIssuePage} />
+        <Route path="/issue" exact={true} component={IssuePage} />
+        <Redirect from="*" to="/issue" />
+      </Switch>
+    </>
   );
 };
 
