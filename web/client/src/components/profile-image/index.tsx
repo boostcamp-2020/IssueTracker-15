c2263e2159600e13dd970156ea3d329c0398998a
moaikang <kk_w1207@naver.com>
2020-11-07 20:34:57 +0900

    feat    : :zap:️ 프로필 이미지 컴포넌트
    프로필 이미지 컴포넌트를 제작하였다.
    프로필 이미지가 없을 경우 기본 이미지가 적용되도록 제작하였다.

---
+++ web/client/src/components/profile-image/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import * as S from './style'
+
+interface ProfileImageProps {
+  img: string | null,
+  size: number
+}
+export default function ProfileImage({img, size} : ProfileImageProps ,) {
+  return (
+    <>
+      <S.UserProfile src={img || "https://i.stack.imgur.com/frlIf.png"} alt='user-profile' size={40}/>
+    </>
+  )
+}
