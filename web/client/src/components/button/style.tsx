b0c5aa8464e1f9e951e539d4127c466278085a13
박진용 <jinyoung1997@gmail.com>
2020-11-10 02:06:56 +0900

    refactor: :rainbow: button 태그 변경

--- web/client/src/components/button/style.tsx
+++ web/client/src/components/button/style.tsx
@@ -1,19 +1,17 @@
 import styled from "styled-components";
 
-export const CustomButton = styled.div`
+export const CustomButton = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-
   border-radius: 6px;
   color: ${(props) => (props.color === "white" ? "black" : "white")};
   background-color: ${(porps) =>
-    porps.color === "white" ? "white" : "#2ea44f"};
+    porps.color === "white" ? "#FAFBFC" : "#2ea44f"};
   width: max-content;
   height: 1.5rem;
   padding: 0.3rem 0.5rem;
-
-  :hover {
-    cursor: pointer;
-  }
+  cursor: pointer;
+  border: none;
+  outline: none;
 `;
