16261f2050fae5c6ea166af48d8f3132afd929d1
박진용 <jinyoung1997@gmail.com>
2020-11-09 17:50:36 +0900

    feat    : ⚡ labelRow 에 key value 추가

--- web/client/src/components/label-row-container/index.tsx
+++ web/client/src/components/label-row-container/index.tsx
@@ -5,15 +5,30 @@ import * as S from "./style";
 
 export default function LabelRowContainer() {
   const labels: LabelForm[] = [
-    { title: "backend", color: "#ff0000", description: "바보같이 굴지말기" },
-    { title: "frontend", color: "#bba333", description: "예쁘게 디자인" },
-    { title: "기어 second", color: "#2ea44f", description: "오늘 달린다" },
+    {
+      id: 1,
+      title: "backend",
+      color: "#ff0000",
+      description: "바보같이 굴지말기",
+    },
+    {
+      id: 2,
+      title: "frontend",
+      color: "#bba333",
+      description: "예쁘게 디자인",
+    },
+    {
+      id: 3,
+      title: "기어 second",
+      color: "#2ea44f",
+      description: "오늘 달린다",
+    },
   ];
 
   return (
     <S.LabelRowContainer>
       {labels.map((label) => {
-        return <LabelRow label={label} />;
+        return <LabelRow key={label.id} label={label} />;
       })}
     </S.LabelRowContainer>
   );
