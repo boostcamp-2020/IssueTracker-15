dee2ffde22851b99c68994ccedf7353c5f1f31b2
Hera <maong0927@gmail.com>
2020-11-09 23:16:19 +0900

    chore   : :package: node_module 변수 제거
    node_module 변수 없이 파일로만 build 가능하게 수정

--- web/client/webpack.dev.js
+++ web/client/webpack.dev.js
@@ -1,10 +1,30 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { merge } = require("webpack-merge");
-const path = require("path");
-
 const common = require("./webpack.common");
 const config = {
   mode: "development",
-
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              // true 로 설정하면 typeCheck x , false 로 설정하면 typeCheck o
+              transpileOnly: false,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
   devServer: {
     contentBase: "./dist/",
     historyApiFallback: true,
--- web/client/webpack.prod.js
+++ web/client/webpack.prod.js
@@ -1,6 +1,35 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
 const config = {
   mode: "production",
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              // true 로 설정하면 typeCheck x , false 로 설정하면 typeCheck o
+              transpileOnly: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      minify: {
+        collapseWhitespace: true,
+        useShortDoctype: true,
+        removeScriptTypeAttributes: true,
+      },
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
 };
 module.exports = merge(common, config);
