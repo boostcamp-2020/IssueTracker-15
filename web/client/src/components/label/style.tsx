2e561831ad3be9345d60f32ad62556d25306d8af
박진용 <jinyoung1997@gmail.com>
2020-11-09 16:42:23 +0900

    feat    : ⚡ label component 제작
    1. label 이름과 color 를 props 로 전달 받기

---
+++ web/client/src/components/label/style.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Label = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.color};
+  width: max-content;
+  padding: 5px;
+  border-radius: 9px;
+`;
