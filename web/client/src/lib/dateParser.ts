e2feca515a5a2c3621d23219db4c0778d8afbfde
moaikang <kk_w1207@naver.com>
2020-11-09 15:40:15 +0900

    refactor: :rainbow: dateParser 함수 리팩토링
    DateParser 함수에 들어있는 상수들을 enum 형식을 사용하여 따로 묶어놔서 숫자가 뭘 의미하는지 쉽게 알 수 있게 리팩토링하였다.
    또한, getDay() 함수를 getDate()로 수정하여 잘못된 로직을 수정하였다.

--- web/client/src/lib/dateParser.ts
+++ web/client/src/lib/dateParser.ts
@@ -1,20 +1,29 @@
 export const getTimeTillNow = (time: string): string => {
+
+  enum Time {
+    MILLISECONDS_PER_SECOND = 1000,
+    SECONDS_PER_MINUTE = 60,
+    MINUTES_PER_HOUR = 60,
+    HOURS_PER_DAY = 24,
+    DAYS_PER_MONTH = 30,
+    MONTHES_PER_YEAR = 12
+  }
+
   const now = new Date();
   const fromTime = new Date(time);
 
+  const minutesTillNow = parseInt(((now.getTime() - fromTime.getTime()) / (Time.MILLISECONDS_PER_SECOND * Time.SECONDS_PER_MINUTE) ).toString());
+  if(minutesTillNow < Time.MINUTES_PER_HOUR) return minutesTillNow.toString() + ' minutes';
 
-  const minutesTillNow = parseInt(((now.getTime() - fromTime.getTime()) / (1000 * 60) ).toString());
-  if(minutesTillNow < 60) return minutesTillNow.toString() + ' minutes';
+  const hoursTillNow = minutesTillNow / Time.MINUTES_PER_HOUR;
+  if(hoursTillNow < Time.HOURS_PER_DAY) return parseInt(hoursTillNow.toString()).toString() + ' hours';
 
-  const hoursTillNow = minutesTillNow / 60;
-  if(hoursTillNow < 24) return parseInt(hoursTillNow.toString()).toString() + ' hours';
+  const daysTillNow = now.getDate() - fromTime.getDate();
+  if(daysTillNow <= Time.DAYS_PER_MONTH) return parseInt(daysTillNow.toString()).toString() + ' days';
 
-  const daysTillNow = now.getDay() - fromTime.getDay();
-  if(daysTillNow < 31) return parseInt(daysTillNow.toString()).toString() + ' days';
+  const monthsTillNow = daysTillNow / Time.DAYS_PER_MONTH;
+  if(monthsTillNow < Time.MONTHES_PER_YEAR) return parseInt(monthsTillNow.toString()).toString() + ' months';
 
-  const monthsTillNow = daysTillNow / 30;
-  if(monthsTillNow < 12) return parseInt(monthsTillNow.toString()).toString() + ' months';
-
-  const yearsTillNow = monthsTillNow / 12;
+  const yearsTillNow = monthsTillNow / Time.MONTHES_PER_YEAR;
   return parseInt(yearsTillNow.toString()).toString() + 'years';
 }
