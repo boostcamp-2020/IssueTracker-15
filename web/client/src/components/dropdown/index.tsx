3c8647e0a08342abfc03779bf0a163022fe348e4
Hera <maong0927@gmail.com>
2020-11-10 12:34:41 +0900

    1. feat    : :zap:️ dropdown wrapper 컴포넌트  구현

--- web/client/src/components/dropdown/index.tsx
+++ web/client/src/components/dropdown/index.tsx
@@ -1,8 +1,36 @@
 import React from "react";
 import * as S from "./style";
+import DropdownContent from "./dropdown-content";
+import DropdownTitle from "./dropdown-title";
 
-function dropdown() {
-  <S.DropdownWrapper>
-    <S.ListWrapper></S.ListWrapper>
-  </S.DropdownWrapper>;
+interface dropdownProps {
+  title: string;
+  contents: string[];
 }
+// { "제목 " : {{"title" : ~}, {"content" : [] }};
+function Dropdown({ title, contents }: dropdownProps) {
+  return (
+    <S.DropdownWrapper>
+      <DropdownTitle title={title} />
+      <S.ListWrapper>
+        {contents.map((content: string) => {
+          <DropdownContent content={content} />;
+        })}
+      </S.ListWrapper>
+    </S.DropdownWrapper>
+  );
+}
+
+export default Dropdown;
+/*
+<S.DropdownWrapper>
+    <ListTitle>Filter Issues </ListTitle>
+    <S.ListWrapper>
+      <ListComp>Open Issues</ListComp>
+      <ListComp>Your Issues </ListComp>
+      <ListComp>Everything assigned to you</ListComp>
+      <ListComp>Everything mentioning to you</ListComp>
+      <ListComp>Closed Issues</ListComp>
+    </S.ListWrapper>
+  </S.DropdownWrapper>;
+*/
