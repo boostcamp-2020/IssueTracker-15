7f626bcf3155eded45e7bdecac3480068a9357fd
moaikang <kk_w1207@naver.com>
2020-11-10 16:10:42 +0900

    refactor: :rainbow: textarea refactoring
    TextArea 태그의 onChange, value 속성을 외부에서 관리하게끔 리팩토링 하였다.

--- web/client/src/components/textarea/index.tsx
+++ web/client/src/components/textarea/index.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import * as S from "./style";
 
-function TextArea() {
+interface TextAreaProps {
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+}
+
+function TextArea({ value, onChange }: TextAreaProps) {
   return (
     <S.TextAreaWrapper>
       <S.StyledTextArea
         autoCapitalize="none"
         autoComplete="off"
         placeholder="leave a comment"
+        value={value}
+        onChange={onChange}
       />
       <S.AttachFileBox>Attach files by clicking here.</S.AttachFileBox>
     </S.TextAreaWrapper>
