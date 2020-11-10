f42b8c9921143a7d8341bcb6281e9b7e05b99e18
박진용 <jinyoung1997@gmail.com>
2020-11-11 00:34:40 +0900

    feat    : ⚡ github oAuth front 구현

--- web/client/src/lib/api.ts
+++ web/client/src/lib/api.ts
@@ -9,3 +9,18 @@ export const getLabels = async () => {
   const labelList = await result.json();
   return labelList;
 };
+
+export const getJWTToken = async (code: string) => {
+  const result = await fetch("http://localhost:3000/api/auth/github", {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      answer: code,
+    }),
+  });
+  const { accessToken } = await result.json();
+  return accessToken;
+};
