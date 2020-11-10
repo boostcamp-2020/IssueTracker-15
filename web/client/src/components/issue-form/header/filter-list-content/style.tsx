622c1fe8b8dffa4e68b524be5744e10d07fbcd3a
Hera <maong0927@gmail.com>
2020-11-10 19:40:23 +0900

    feat    : :zap:️ filter 목록 컴포넌트 구현
    1. filter 목록 컴포넌트 구현

---
+++ web/client/src/components/issue-form/header/filter-list-content/style.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const FilterListComp = styled.div`
+  position: relative;
+  color: #586069;
+  margin-left: 3%;
+`;
--- web/client/src/components/issue-form/header/index.tsx
+++ web/client/src/components/issue-form/header/index.tsx
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
 import * as S from "./style";
+import FilterListComp from "./filter-list-content";
 
 // { "제목 " : {{"title" : ~}, {"content" : [] }};
 function issueHeader() {
   return (
     <S.IssueHeader>
       <input type="checkbox" name="xxx" value="yyy" checked />
+      <S.FilterList>
+        <FilterListComp title="Author"></FilterListComp>
+        <FilterListComp title="Label"></FilterListComp>
+        <FilterListComp title="Projects"></FilterListComp>
+        <FilterListComp title="Milestones"></FilterListComp>
+        <FilterListComp title="Assignee"></FilterListComp>
+        <FilterListComp title="sort"></FilterListComp>
+      </S.FilterList>
     </S.IssueHeader>
   );
 }
