bbeb9fdbde93c2daaf67ded33bac6b132601af36
박진용 <jinyoung1997@gmail.com>
2020-11-10 18:23:10 +0900

    refactor : :rainbow: label 목록 fetch api 분리

--- web/client/src/views/label/index.tsx
+++ web/client/src/views/label/index.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import LabelRowContainer from "../../containers/label-row-container";
 import LabelListHeader from "../../containers/label-list-header";
 
+interface LabelContextProps {
+  createToggle: boolean;
+}
+
+export const LabelContext = React.createContext({} as LabelContextProps);
+
 const LabelPage = () => {
   return (
-    <>
+    <LabelContext.Provider value={{ createToggle: false }}>
       <LabelListHeader />
       <LabelRowContainer />
-    </>
+    </LabelContext.Provider>
   );
 };
 
