29c4a45a092395d5bf24ce820b438bb16b2f3aff
강근우 <52201658+moaikang@users.noreply.github.com>
2020-11-10 23:40:35 +0900

    Revert "[FE]  GitHub OAuth 프론트 처리"

--- web/client/src/components/auth/login.tsx
+++ web/client/src/components/auth/login.tsx
@@ -1,14 +1,7 @@
 import React, { Fragment } from "react";
 import Input from "./input-form";
 import Button from "./button";
-import GithubButton from "react-github-login-button";
 import * as S from "./style";
-import styled from "styled-components";
-
-const GitHubBTN = styled(GithubButton)`
-  margin-top: 10% !important;
-  width: 100% !important;
-`;
 
 function Login() {
   return (
@@ -19,13 +12,6 @@ function Login() {
         <Button btnName="로그인" />
         <Button btnName="회원가입" />
       </S.WrapBtnForm>
-      <GitHubBTN
-        onClick={() => {
-          window.open(
-            "https://github.com/login/oauth/authorize?client_id=bbd8f4faffe4f6f9c9b7&redirect_uri=http://localhost:8080/auth/github"
-          );
-        }}
-      />
     </Fragment>
   );
 }
