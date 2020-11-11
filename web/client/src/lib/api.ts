ff343a373314a96d5f58b69448c0f48074b43dfb
moaikang <kk_w1207@naver.com>
2020-11-11 22:55:46 +0900

    feat    : :zap:️ 이슈 상세 페이지에 필요한 API 일부 작성
    이슈 상세 페이지에 필요한 API 일부 작성하였다.

--- web/client/src/lib/api.ts
+++ web/client/src/lib/api.ts
@@ -9,3 +9,33 @@ export const getLabels = async () => {
   const labelList = await result.json();
   return labelList;
 };
+
+export const getIssueById = async (id: number) => {
+  const res = await fetch(
+    `${URL}/api/issue/${id}`
+  );
+
+  if(!res.ok) return;
+  const issue = await res.json();
+  return issue;
+};
+
+interface CommentProps {
+	  userId : number;
+	  issueId : number;
+    content :string;
+}
+
+export const postComment = async ({userId, issueId, content}: CommentProps ) => {
+  const res = await fetch(`${URL}/api/`, {
+    method: 'POST', 
+    body: JSON.stringify({userId, issueId, content}), 
+    headers:{
+      'Content-Type': 'application/json'
+    }
+  })
+
+  if(!res.ok) return;
+  return await res.json();
+}
+
