5ae5c3aaf85e606ee3965e6f21f01304630045a7
sihyungyou <sihyungyou@gmail.com>
2020-11-12 20:25:31 +0900

    feat :⚡️ 레이블 배경색에 따른 글자색 white/black 구현

--- iOS/IssueTracker/IssueTracker/02.LabelScene/View/LabelCell/LabelCellView.swift
+++ iOS/IssueTracker/IssueTracker/02.LabelScene/View/LabelCell/LabelCellView.swift
@@ -27,7 +27,10 @@ class LabelCellView: UICollectionViewCell {
     }
     
     func configureTitle(text: String, color: CGColor) {
+        let uiColor = UIColor(cgColor: color)
+        
         titleLabel.text = text
+        titleLabel.textColor = uiColor.isDarkColor ? UIColor.white : UIColor.black
         titleLabel.setBackgroundColor(color)
         titleLabel.cornerRadiusRatio = 0.5
         titleLabel.setPadding(top: 3, left: 5, bottom: 3, right: 5)
@@ -49,5 +52,15 @@ extension LabelCellView: UICollectionViewRegisterable {
 
     static var cellNib: UINib {
         return UINib(nibName: "LabelCellView", bundle: nil)
+    }
+}
+
+extension UIColor {
+    var isDarkColor: Bool {
+        var r, g, b, a: CGFloat
+        (r, g, b, a) = (0, 0, 0, 0)
+        self.getRed(&r, green: &g, blue: &b, alpha: &a)
+        let lum = 0.2126 * r + 0.7152 * g + 0.0722 * b
+        return  lum < 0.50
     }
 }
